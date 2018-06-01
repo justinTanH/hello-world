96b07626c0cce3b01b0b3b45a0b5a09ef451e892
justinTanH <zhtbth@163.com>
2018-06-01 10:31:16 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public Class Test{
   public static void main（String[]args）{
     System.out.println("hello world!");
     System.out.println("i dont believe on you!");
+    System.out.println("success!");
   }
 }
